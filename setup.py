17072c2472e9850e667bd45206ab6b980c2bccac
Andrew McPherson <andrew.mcpherson@gmail.com>
2014-03-10 11:54:46 -0700

    improved setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import fnmatch
 import os
 import sys
+import platform
 import argparse
 
 from distutils.core import setup
@@ -11,20 +12,36 @@ argparser.add_argument('--boost_source', help='boost source directory', required
 args, unknown = argparser.parse_known_args()
 sys.argv = [sys.argv[0]] + unknown
 
-def get_filenames(directory, filter):
-    directory = os.path.abspath(os.path.expanduser(directory))
-    for root, dirnames, filenames in os.walk(directory):
-        for filename in fnmatch.filter(filenames, filter):
-            yield os.path.join(root, filename)
+boost_source = os.path.expanduser(args.boost_source)
 
-boost_python_source = list(get_filenames(args.boost_source + '/libs/python/src', '*.cpp'))
-boost_serialization_source = list(get_filenames(args.boost_source + '/libs/serialization/src', '*.cpp'))
+def find_files(directory, pattern):
+    for root, dirs, files in os.walk(directory):
+        for basename in files:
+            if fnmatch.fnmatch(basename, pattern):
+                filename = os.path.join(root, basename)
+                yield filename
+
+boost_python_sources = list(find_files(os.path.join(boost_source, 'libs/python/src'), '*.cpp'))
+boost_serialization_sources = list(find_files(os.path.join(boost_source, 'libs/serialization/src'), '*.cpp'))
+
+assert(len(boost_python_sources) > 0)
+assert(len(boost_serialization_sources) > 0)
+
+extra_compile_args = ['-Wno-unused-variable']
+if platform.platform().startswith('Darwin'):
+    extra_compile_args.append('-Wno-unneeded-internal-declaration')
+    extra_compile_args.append('-Wno-unused-private-field')
+
+extra_compile_args.append('-ftemplate-depth-1024')
+
+extra_link_args = []
+if platform.platform().startswith('Darwin'):
+    extra_link_args.append('-Wl,-no_compact_unwind')
 
 ext_modules = [Extension('pygenes',
-                         ['src/pygenes.cpp'] + boost_python_source + boost_serialization_source, 
+                         ['src/pygenes.cpp'] + boost_python_sources + boost_serialization_sources,
                          language='c++',
-                         include_dirs=['src', args.boost_source],
-                         extra_compile_args=['-ftemplate-depth-1024'])]
+                         include_dirs=['src', boost_source],
+                         extra_compile_args=extra_compile_args)]
 
 setup(name='pygenes', ext_modules=ext_modules)
-
