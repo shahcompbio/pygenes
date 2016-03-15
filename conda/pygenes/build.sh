36c2f2c9183daab831065313293dd29a91277a7d
Andrew McPherson <amcphers@bccrc.ca>
2016-03-15 16:43:14 -0700

    build: conda boost source dependency

--- conda/pygenes/build.sh
+++ conda/pygenes/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-wget https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz
-tar -xzvf boost_1_60_0.tar.gz
-$PYTHON setup.py install --boost_source boost_1_60_0
+$PYTHON setup.py install --boost_source $PREFIX/src/boost/
+
