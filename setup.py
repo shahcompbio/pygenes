50066cbb169d8d41cc87fe7d56994cd6267c0484
Andrew McPherson <andrew.mcpherson@gmail.com>
2019-08-11 10:23:36 -0700

    build: added manylinux build

--- setup.py
+++ setup.py
@@ -7,26 +7,6 @@ import argparse
 from distutils.core import setup
 from distutils.extension import Extension
 
-argparser = argparse.ArgumentParser(add_help=False)
-argparser.add_argument('--boost_source', help='boost source directory', required=True)
-args, unknown = argparser.parse_known_args()
-sys.argv = [sys.argv[0]] + unknown
-
-boost_source = os.path.expanduser(args.boost_source)
-
-def find_files(directory, pattern):
-    for root, dirs, files in os.walk(directory):
-        for basename in files:
-            if fnmatch.fnmatch(basename, pattern):
-                filename = os.path.join(root, basename)
-                yield filename
-
-boost_python_sources = list(find_files(os.path.join(boost_source, 'libs/python/src'), '*.cpp'))
-boost_serialization_sources = list(find_files(os.path.join(boost_source, 'libs/serialization/src'), '*.cpp'))
-
-assert(len(boost_python_sources) > 0)
-assert(len(boost_serialization_sources) > 0)
-
 extra_compile_args = ['-Wno-unused-variable']
 if platform.platform().startswith('Darwin'):
     extra_compile_args.append('-Wno-unneeded-internal-declaration')
@@ -41,9 +21,9 @@ if platform.platform().startswith('Darwin'):
 ext_modules = [
     Extension(
         'pygenes',
-        ['src/pygenes.cpp'] + boost_python_sources + boost_serialization_sources,
+        ['src/pygenes.cpp'],
         language='c++',
-        include_dirs=['src', boost_source],
+        include_dirs=['src'],
         extra_compile_args=extra_compile_args),
 ]
 
---
+++ travis/build-wheels.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e -x
+
+# Install boost
+yum install -y wget
+tar -xzvf boost_1_60_0.tar.gz
+cd boost_1_60_0
+./bootstrap.sh
+./b2 install
+cd ..
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install -r /io/dev-requirements.txt
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin/; do
+    "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
+    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+done
